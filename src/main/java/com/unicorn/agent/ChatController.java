43f6114f7ff9306f37c9c773896bf02e3314ae40
mediben <workshop-user@example.com>
2025-10-06 12:57:54 +0000

    Add external memory

--- src/main/java/com/unicorn/agent/ChatController.java
+++ src/main/java/com/unicorn/agent/ChatController.java
@@ -2,11 +2,16 @@ package com.unicorn.agent;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.chat.memory.repository.jdbc.PostgresChatMemoryRepositoryDialect;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.sql.DataSource;
 
 import reactor.core.publisher.Flux;
 
@@ -16,32 +21,34 @@ public class ChatController {
 
     private static final String DEFAULT_SYSTEM_PROMPT = """
 		You are a helpful AI assistant for Unicorn Rentals, a fictional company that rents unicorns.
-		Be friendly, helpful, and concise in your responses. But be direct and sracstic.
+		Be friendly, helpful, and concise in your responses.
 		""";
 
 	private final ChatClient chatClient;
 
-	public ChatController (ChatClient.Builder chatClient){
+	public ChatController (ChatClient.Builder chatClient, DataSource dataSource){
+		var chatMemoryRepository = JdbcChatMemoryRepository.builder()
+			.dataSource(dataSource)
+			.dialect(new PostgresChatMemoryRepositoryDialect())
+			.build();
 
-        var chatMemory = MessageWindowChatMemory.builder()
+		var chatMemory = MessageWindowChatMemory.builder()
+			.chatMemoryRepository(chatMemoryRepository)
 			.maxMessages(20)
 			.build();
 
-        this.chatClient = chatClient
+		this.chatClient = chatClient
 			.defaultSystem(DEFAULT_SYSTEM_PROMPT)
-            .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+			.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
 			.build();
 	}
 
-    @PostMapping("chat")
-    public String chat(@RequestBody PromptRequest promptRequest){
-        var chatResponse = chatClient.prompt().user(promptRequest.prompt()).call().chatResponse();
-        return (chatResponse != null) ? chatResponse.getResult().getOutput().getText() : null;
-	}
-
-    @PostMapping("/chat/stream")
+	@PostMapping("/chat/stream")
 	public Flux<String> chatStream(@RequestBody PromptRequest promptRequest){
-		return chatClient.prompt().user(promptRequest.prompt()).stream().content();
+		var conversationId = "user1"; //This should be retrieved from the Auth context
+		return chatClient.prompt().user(promptRequest.prompt())
+			.advisors(advisor -> advisor.param(ChatMemory.CONVERSATION_ID, conversationId))
+			.stream().content();
 	}
 
     record PromptRequest(String prompt) {
