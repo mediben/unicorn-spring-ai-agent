d03737c0234e428d50e67e25d828644a26f58c5c
mediben <workshop-user@example.com>
2025-10-06 12:21:38 +0000

    add chat client

--- src/main/java/com/unicorn/agent/AgentApplication.java
+++ src/main/java/com/unicorn/agent/AgentApplication.java
@@ -1,13 +1,37 @@
 package com.unicorn.agent;
 
+import java.util.Scanner;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgentApplication {
 
+	@Bean
+    public CommandLineRunner cli(ChatClient.Builder chatClientBuilder) {
+        return args -> {
+            var chatClient = chatClientBuilder
+                .defaultSystem("You are a Unicorn Rentals Agent, expert in all sorts of things related to Unicorns and renting them.")
+                .build();
+
+            System.out.println("\nI am your Unicorn Rentals assistant.\n");
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    System.out.print("\nUSER: ");
+                    System.out.println("\nASSISTANT: " +
+                        chatClient.prompt(scanner.nextLine()) // Get the user input
+                            .call()
+                            .content());
+                }
+            }
+        };
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(AgentApplication.class, args);
 	}
-
 }
